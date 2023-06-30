ab115a5484a331fdc94ec14a97fa400a4dba32d1
Jean Fredrik Leirtrø <jealei@gmail.com>
2023-06-30 12:49:50 +0200

    tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 function App() {
   const [currentYear, setCurrentYear] = useState(2023);
   const [isWeekend, setIsWeekend] = useState(false);
+  const [countdown, setCountdown] = useState("0d 0t 0m");
+  const [timeRemaining, setTimeRemaining] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -12,6 +14,39 @@ function App() {
       const currentHour = currentDate.getHours();
       const currentYear = currentDate.getFullYear();
       const currentMinute = currentDate.getMinutes();
+      const currentSecond = currentDate.getSeconds();
+      const targetDay = 5;
+      const targetHour = 16;
+
+      let daysRemaining = targetDay - currentDay;
+      let hoursRemaining = targetHour - currentHour;
+      let minutesRemaining = 59 - currentMinute;
+      let secondsRemaining = 59 - currentSecond;
+
+      if (daysRemaining < 0) {
+        daysRemaining += 7;
+      }
+      if (hoursRemaining < 0) {
+        hoursRemaining += 24;
+        daysRemaining -= 1;
+      }
+      if (minutesRemaining < 0) {
+        minutesRemaining += 60;
+        hoursRemaining -= 1;
+      }
+      if (secondsRemaining < 0) {
+        secondsRemaining += 60;
+        minutesRemaining -= 1;
+      }
+
+      const countdownText = `${daysRemaining}d ${hoursRemaining}t ${minutesRemaining}m ${secondsRemaining}s`;
+      setCountdown(countdownText);
+      setTimeRemaining({
+        days: daysRemaining,
+        hours: hoursRemaining,
+        minutes: minutesRemaining,
+        seconds: secondsRemaining,
+      });
 
       setCurrentYear(currentYear);
 
@@ -33,21 +68,46 @@ function App() {
   return (
     <Fragment>
       <div className="container">
-        <h1>Er det helg?</h1>
+        <h1 className="title">Er det helg?</h1>
         {isWeekend ? <p className="yes">Ja!</p> : <p className="no">Nei</p>}
+        {!isWeekend && (
+          <div className="countdown">
+            <p className="countdown-text">Det er helg om:</p>
+            <div className="countdown-time">
+              <div className="countdown-row">
+                <p className="countdown-value">{timeRemaining.days}</p>
+                <p className="countdown-label">dager</p>
+              </div>
+              <div className="countdown-row">
+                <p className="countdown-value">{timeRemaining.hours}</p>
+                <p className="countdown-label">timer</p>
+              </div>
+              <div className="countdown-row">
+                <p className="countdown-value">{timeRemaining.minutes}</p>
+                <p className="countdown-label">minutter</p>
+              </div>
+              <div className="countdown-row countdown-seconds">
+                <p className="countdown-value">{timeRemaining.seconds}</p>
+                <p className="countdown-label">sekunder</p>
+              </div>
+            </div>
+          </div>
+        )}
         {isWeekend ? (
           <p className="ad">Og du kan starte helgen med å se Ramona's Tea Party sin nye musikkvideo!</p>
         ) : (
-          <p className="ad">Men da kan du se Ramona's Tea Party sin nye musikkvideo mens du venter!</p>
+          <p className="ad">I mellomtiden kan du se Ramona's Tea Party sin nye musikkvideo mens du venter!</p>
         )}
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/UeGjhDvrAKk"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+        <div className="video-container">
+          <iframe
+            width="448"
+            height="252"
+            src="https://www.youtube.com/embed/UeGjhDvrAKk"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
       <footer className="footer">
         <p>&copy; {currentYear} Chris November. All rights reserved.</p>
